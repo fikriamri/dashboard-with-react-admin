fa0932c7015f9585dc6ac49e0caae078da3234a6
fikriamri <famri@alterra.id>
2020-07-24 16:09:10 +0700

    update folder structure for better development experience

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
 import { Admin, Resource, ListGuesser, EditGuesser, fetchUtils } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import { createHashHistory } from 'history';
+import englishMessages from 'ra-language-english';
+import indonesianMessages from 'ra-language-indonesian';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import createAdminStore from './redux/createAdminStore';
+
 import authProvider from './authProvider';
-import { UserList } from './users';
-import { BookList } from './books';
-import { PostList, PostEdit, PostCreate } from './posts';
+import { UserList } from './resources/users';
+import { BookList } from './resources/books';
+import { PostList, PostEdit, PostCreate } from './resources/posts';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import CollectionsBookmarkIcon from '@material-ui/icons/CollectionsBookmark';
-import customDataProvider from './customDataProvider';
-import Dashboard from './Dashboard';
+import customDataProvider from './dataProvider';
+import Dashboard from './pages/Dashboard';
 import customRoutes from './routes';
-import Menu from './Menu';
+import Menu from './menu';
 
-const httpClient = (url, options = {}) => {
-  if (!options.headers) {
-      options.headers = new Headers({ Accept: 'application/json' });
-  }
-  const token = localStorage.getItem('token');
-  options.headers.set('Authorization', `Bearer ${token}`);
-  return fetchUtils.fetchJson(url, options);
+const messages = {
+  id: indonesianMessages,
+  en: englishMessages,
 };
 
-const dataProvider = jsonServerProvider('http://localhost:3000/api', httpClient);
+const i18nProvider = polyglotI18nProvider(locale => {
+  if (locale !== 'en') {
+      return messages[locale];
+  }
+  return englishMessages;
+});
+const history = createHashHistory();
+
 const App = () => (
-  <Admin
-    menu={Menu}
-    dashboard={Dashboard}
-    authProvider={authProvider}
-    dataProvider={customDataProvider}
-    customRoutes={customRoutes}
+  <Provider
+    store={createAdminStore({
+      authProvider,
+      customDataProvider,
+      history
+    })}
   >
-    <Resource
-      name="book"
-      list={BookList}
-      icon={CollectionsBookmarkIcon}
-    />
-  </Admin>
+    <Admin
+      menu={Menu}
+      dashboard={Dashboard}
+      authProvider={authProvider}
+      dataProvider={customDataProvider}
+      customRoutes={customRoutes}
+      history={history}
+    >
+      <Resource
+        name="book"
+        list={BookList}
+        icon={CollectionsBookmarkIcon}
+      />
+    </Admin>
+  </Provider>
 )
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Faq from './Faq';
+import Faq from '../pages/Faq';
 
 export default [
     <Route exact path="/faq" component={Faq} />];
